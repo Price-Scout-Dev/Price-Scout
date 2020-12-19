f2a0150de0c25744f929eb9e09f04b2d01b957a4
Stanley Huang <iamstanleyhuang@gmail.com>
2020-12-19 13:30:01 -0800

    Add boiler code

---
+++ client/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main = ({email, password}) => {
+  return (
+    <>
+      <h3>Main</h3>
+      <h1>{email}</h1>
+      <h1>{password}</h1>
+    </>
+  )
+}
+
+export default Main;
