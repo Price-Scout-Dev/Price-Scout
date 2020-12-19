5e54ccd1df766ed1bcc09b73e1b60645b0050b3b
Stanley Huang <iamstanleyhuang@gmail.com>
2020-12-19 10:05:58 -0800

    Change import statements

--- client/SANDBOX/nav/LogInNavBar.jsx
+++ client/SANDBOX/nav/LogInNavBar.jsx
@@ -1,14 +1,11 @@
-import React from './node_modules/react';
-import { Link } from './node_modules/react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LogInNavBar = () => (
-
-  <nav>
-    <Link to='/'>Register</Link>
-    <Link to='/about'>About</Link>
-  </nav>
-  
+	<nav>
+		<Link to="/">Register</Link>
+		<Link to="/about">About</Link>
+	</nav>
 );
-
 
 export default LogInNavBar;
--- client/SANDBOX/nav/NavBar.jsx
+++ client/SANDBOX/nav/NavBar.jsx
@@ -1,32 +1,30 @@
-import React from './node_modules/react';
+import React from 'react';
 import AboutNavBar from './AboutNavBar';
 import LogInNavBar from './LogInNavBar';
 import RegNavBar from './RegNavBar';
-import { Link } from './node_modules/react-router-dom';
+import { Link } from 'react-router-dom';
 
 const NavBar = ({ match }) => {
-   
-  if (match.params === 'login') return (
-    <>
-      <AboutNavBar />
-      <RegNavBar />
-    </>
-  )
+	// if (match.params === 'login') return (
+	//   <>
+	//     <AboutNavBar />
+	//     <RegNavBar />
+	//   </>
+	// )
 
-  if (match.params === 'about') return (
-    <>
-      <AboutNavBar />
-      <LogInNavBar />
-    </>
-  )
+	// if (match.params === 'about') return (
+	//   <>
+	//     <AboutNavBar />
+	//     <LogInNavBar />
+	//   </>
+	// )
 
-  return (
-    <>
-      <AboutNavBar />
-      <LogInNavBar />
-    </>
-  )
+	return (
+		<>
+			<AboutNavBar />
+			<LogInNavBar />
+		</>
+	);
 };
-
 
 export default NavBar;
