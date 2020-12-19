881e380116b71e1b06b79bc8a7b0e450448fc572
Stanley Huang <iamstanleyhuang@gmail.com>
2020-12-19 15:00:33 -0800

    Clean up email and password validation syntax

--- client/components/auth/Login.jsx
+++ client/components/auth/Login.jsx
@@ -12,8 +12,9 @@ const Login = ({ loginUser }) => {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
-		if (!emailInput) return alert('Fill in the email input please!')
-		else if (pwInput.length < 5) return alert('Enter password (must be at least 5 characters long)!');
+		if (!emailInput) return alert('Fill in the email input please!');
+		else if (pwInput.length < 5)
+			return alert('Enter password (must be at least 5 characters long)!');
 		else if (!emailInput.includes('@') || !emailInput.includes('.com'))
 			return alert('Invalid email format. Try again!');
 
