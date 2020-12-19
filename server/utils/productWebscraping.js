3ed8dfe7aae54db1f0c574ab0db85b35b114e0b7
Chandni Patel <chandnipatel@Chandnis-Mac-mini.local>
2020-12-19 17:31:51 -0500

    updated productWebscraping file

--- server/utils/productWebscraping.js
+++ server/utils/productWebscraping.js
@@ -1,13 +1,38 @@
+const puppeteer = require("puppeteer");
 
-const puppeteer = require('puppeteer');
-const getPrice = async (url) => {
+const getProductInfo = async (url) => {
   const browser = await puppeteer.launch({
-    args: ['--disabled-setuid-sandbox', '--no-sandbox'],
+    args: ["--disabled-setuid-sandbox", "--no-sandbox"],
   });
   const page = await browser.newPage();
   await page.goto(url);
-  await page.waitForSelector('.g9WBQb');
-  const price = await page.$eval('.g9WBQb', (el) => el.innerHTML);
-  console.log('Price: ', price);
+
+
+  //1. Get lowestDailyPrice 
+  await page.waitForSelector(".g9WBQb");
+  const price = await page.$eval(".g9WBQb", (el) => el.innerHTML);
+  console.log("Price: ", price);
+
+  //2. Get storeName where lowestDailyPrice is from. 
+  const storeName = await page.$eval(".BvQan", (el) => el.innerHTML);
+  console.log("storeName: ", storeName);
+
+  //3. Get storeUrl of the store 
+
+
+  //4. Get productName 
+
+
+
+  //5. Get productImageUrl 
+
+
+
+
+
 };
-getPrice('https://www.google.com/shopping/product/9333373489557772337/');
+
+getProductInfo('https://www.google.com/shopping/product/9333373489557772337/')
+//sample product page: https://www.google.com/shopping/product/9333373489557772337/
+
+module.exports = getProductInfo;
