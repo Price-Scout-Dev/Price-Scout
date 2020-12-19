67fd52f7c16dca6a7113dcedd63cdff55b6be553
Stanley Huang <iamstanleyhuang@gmail.com>
2020-12-19 13:30:09 -0800

    Change from div to fragment

--- client/components/about/About.jsx
+++ client/components/about/About.jsx
@@ -3,12 +3,12 @@ import AboutNavBar from '../nav/AboutNavBar';
 import { AppBar } from '@material-ui/core';
 
 const About = () => (
-  <div>
+  <>
     <AppBar>
       <AboutNavBar />
     </AppBar>
     <h1>Hi, i am the about page</h1>
-  </div>
+  </>
 );
 
 export default About;
