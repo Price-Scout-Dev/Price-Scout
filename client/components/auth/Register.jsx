5e3468ec20e8cd8fba1617f65298f40097304a73
Stanley Huang <iamstanleyhuang@gmail.com>
2020-12-19 15:01:07 -0800

    Clean up email and password validation syntax

--- client/components/auth/Register.jsx
+++ client/components/auth/Register.jsx
@@ -5,18 +5,20 @@ import { Button, Box, AppBar } from '@material-ui/core';
 import { TextField } from '@material-ui/core';
 
 const Register = ({ registerUser }) => {
-	//GET THE USER INPUT SO WE CAN ISSUE A POST TO DB W/ postUser, AND THEN UPDATE APP STATE W/ setUser
-
 	const [emailInput, updateEmail, resetEmail] = useInput('');
 	const [pwInput, updatePw, resetPw] = useInput('');
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
-		//if emailInput and pwInput are cool
+		if (!emailInput) return alert('Fill in the email input please!');
+		else if (pwInput.length < 5)
+			return alert('Enter password (must be at least 5 characters long)!');
+		else if (!emailInput.includes('@') || !emailInput.includes('.com'))
+			return alert('Invalid email format. Try again!');
 
 		registerUser(emailInput, pwInput);
-	
+
 		resetEmail();
 		resetPw();
 	};
@@ -34,7 +36,7 @@ const Register = ({ registerUser }) => {
 				border={1}
 			>
 				<div>
-					<form onSubmit={handleSubmit}>
+					<form>
 						<h1>Register</h1>
 						<TextField
 							id="email"
@@ -51,7 +53,7 @@ const Register = ({ registerUser }) => {
 							value={pwInput}
 							onChange={updatePw}
 						/>
-						<Button variant="contained" color="primary">
+						<Button onClick={handleSubmit} variant="contained" color="primary">
 							Submit
 						</Button>
 					</form>
