b581c7a8e3bc23b87d31c8a84e0212cc70efda15
Stanley Huang <iamstanleyhuang@gmail.com>
2020-12-19 14:59:42 -0800

    Add useEffect & GET request to /api/products/:userId

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ProductList from './product/ProductList'
+import Search from './search/Search';
 
 const Main = ({email, password}) => {
+
+  const [list, setList] = useState([]);
+
+  const searchProducts = () => {
+    console.log('hi');
+  }
+
+  //cdm
+  useEffect(() => {
+    fetch('/api/products/:userId', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'Application/JSON',
+			},
+			body: JSON.stringify({ email, password }),
+		})
+			.then((res) => res.json())
+			.then((res) => setList(res.TheList)) // res.body?
+			.catch((err) => console.log('ERROR: ', err));
+		setPassword(password);
+  }, []);
+
   return (
     <>
-      <h3>Main</h3>
-      <h1>{email}</h1>
-      <h1>{password}</h1>
+      <h3>Search will be here</h3>
+      <ProductList list={list} />
     </>
   )
 }
