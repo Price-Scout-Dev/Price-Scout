efae614ad023e9dc30a46bbf8006fcd12d5f1ff6
Chandni Patel <chandnipatel@Chandnis-Mac-mini.local>
2020-12-19 15:54:48 -0500

    Added sessions controller to authControllers file and created a sessions table

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 
 const authController = {};
 
+//Create User Controller:
 authController.createUser = (req, res, next) => {
   if (req.body.email.length > 0 && req.body.password.length > 0) {
     //create query string. insert user into the user table.
@@ -10,18 +11,19 @@ authController.createUser = (req, res, next) => {
     INSERT INTO users ( hashedPassword, email) VALUES ($1, $2) RETURNING *
     `; // parameterized sql query
     let values = [req.body.password, req.body.email];
+
     priceTrackerDB
       .query(queryString, values)
       .then((data) => {
         // console.log(data);
-        res.locals.loginInfo = {}
-        res.locals.loginInfo.userid = data.rows[0].userid;
-        res.locals.loginInfo.email = req.body.email
+        res.locals.loginInfo = {};
+        res.locals.loginInfo.userId = data.rows[0].userid;
+        res.locals.loginInfo.email = req.body.email;
         return next();
       })
       .catch((err) => {
         console.log(err);
-        next(err);
+        return next(err);
       });
   } else {
     console.log("password or username rejected");
@@ -29,6 +31,33 @@ authController.createUser = (req, res, next) => {
   }
 };
 
+//Cookie Controller:
+authController.setSSIDCookie = (req, res, next) => {
+  //First, set cookie on the client to a random number
+  let randomNumber = Math.floor(Math.random() * 1000000);
+  let options = { maxAge: 90000000, httpOnly: true };
 
+  res.cookie("ssid", randomNumber, options);
+
+  //second, save the ssid into the database.
+
+  let queryString = `
+  INSERT INTO sessions ( userId, ssid) VALUES ($1, $2) RETURNING *
+  `; // parameterized sql query
+  let values = [res.locals.loginInfo.userId, randomNumber];
+
+  priceTrackerDB
+    .query(queryString, values)
+    .then((data) => {
+      console.log("Session added: ", data);
+      return next();
+    })
+    .catch((err) => {
+      console.log(err);
+      return next(err);
+    });
+
+  return next();
+};
 
 module.exports = authController;
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -9,14 +9,22 @@ const authController = require("../controllers/authControllers.js");
 //req.body has {email, password}.
 //1: create User
 //2: setSSIDCookie
-//3: startSession
+  //Eelan's suggestion: 
+  //set a ssid cookie in the browser expires after x hours. store that ssid in the database (never expires). 
 
-authRouter.post("/signup", authController.createUser, (req, res) => {
+
+authRouter.post("/signup", authController.createUser, authController.setSSIDCookie, (req, res) => {
   res.status(200).json(res.locals.loginInfo);
 });
 
 //Login Route
 //POST Request
+
+
+
+
+
+
 authRouter.post("/login", (req, res) => {
   //what to send if successful?
   res.status(200).json(res.locals.loginInfo); //contains {email, userId}
