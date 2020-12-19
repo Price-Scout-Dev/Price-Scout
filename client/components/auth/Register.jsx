ed2183018eb5b22f1bafa311ea31218f76d196e3
Stanley Huang <iamstanleyhuang@gmail.com>
2020-12-19 13:30:49 -0800

    Refactor handleSubmit and TextFields

--- client/components/auth/Register.jsx
+++ client/components/auth/Register.jsx
@@ -4,51 +4,58 @@ import RegNavBar from '../nav/RegNavBar';
 import { Button, Box, AppBar } from '@material-ui/core';
 import { TextField } from '@material-ui/core';
 
-const Register = ({ setUser, postUser }) => {
+const Register = ({ registerUser }) => {
 	//GET THE USER INPUT SO WE CAN ISSUE A POST TO DB W/ postUser, AND THEN UPDATE APP STATE W/ setUser
 
 	const [emailInput, updateEmail, resetEmail] = useInput('');
 	const [pwInput, updatePw, resetPw] = useInput('');
 
-	const formSubmit = (e) => {
+	const handleSubmit = (e) => {
 		e.preventDefault();
 
-		postUser(emailInput, pwInput);
-		setUser(emailInput, pwInput);
+		//if emailInput and pwInput are cool
 
+		registerUser(emailInput, pwInput);
+	
 		resetEmail();
 		resetPw();
 	};
 
 	return (
 		<>
-		<AppBar>
-      <RegNavBar />
-    </AppBar>
-		<Box
-			display="flex"
-			justifyContent="center"
-			alignItems="center"
-			minHeight="80vh"
-			border={1}
-		>
-			<div>
-				<form onSubmit={formSubmit}>
-					<h1>Register</h1>
-					{/* <input value={emailInput} onChange={updateEmail} placeholder="email" /> */}
-					<TextField id="email" label="email" variant="outlined" />
-					{/* <input
-						type="password"
-						value={pwInput}
-						onChange={updatePw}
-						placeholder="password"
-					/> */}
-					<TextField id="password" label="password" variant="outlined" />
-					<Button variant="contained" color="primary">
-						Submit
-					</Button>
-				</form>
-			</div>
+			<AppBar>
+				<RegNavBar />
+			</AppBar>
+			<Box
+				display="flex"
+				justifyContent="center"
+				alignItems="center"
+				minHeight="80vh"
+				border={1}
+			>
+				<div>
+					<form onSubmit={handleSubmit}>
+						<h1>Register</h1>
+						<TextField
+							id="email"
+							label="Email"
+							variant="outlined"
+							value={emailInput}
+							onChange={updateEmail}
+						/>
+						<TextField
+							id="password"
+							label="Password"
+							variant="outlined"
+							type="password"
+							value={pwInput}
+							onChange={updatePw}
+						/>
+						<Button variant="contained" color="primary">
+							Submit
+						</Button>
+					</form>
+				</div>
 			</Box>
 		</>
 	);
