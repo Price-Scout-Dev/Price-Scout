6ef2a9cbf733e988543678569b804eb8da4ed4ae
Chandni Patel <chandnipatel@Chandnis-Mac-mini.local>
2020-12-21 22:54:42 -0500

    Fixed SQL table labeling

--- server/models/SQLTables.js
+++ server/models/SQLTables.js
@@ -3,20 +3,21 @@ const productDB = require("./priceTrackerModel");
 //CREATE PRODUCTS TABLE:
 // async function createProductsTable() {
 //   let queryString = `
-//   CREATE TABLE products (
-//     productId INT NOT NULL UNIQUE ,
-//     productName VARCHAR NOT NULL,
-//     imageUrl VARCHAR NOT NULL,
-//     PRIMARY KEY (productId)
+//   CREATE TABLE sessions (
+//     _id SERIAL,
+//     user_id INT NOT NULL references users(_id),
+//     ssid INT NOT NULL,
+//     PRIMARY KEY (_id)
 //   )`;
 //   try {
-//     const result = productDB.query(queryString);
+//     const result =  productDB.query(queryString);
 //     console.log(result);
 //   } catch (err) {
 //     console.log(err);
 //   }
 // }
 
+// createProductsTable()
 //CREATE LOWEST DAILY PRICE TABLE:
 // async function createLowestDailyPriceTable() {
 //   let queryString = `
@@ -55,36 +56,38 @@ const productDB = require("./priceTrackerModel");
 // }
 
 // CREATE USER TO PRODUCT TABLE:
-async function usersToProductsTable() {
-  let queryString = ` 
-  CREATE TABLE userstoproducts (
-    usersToProducts_id SERIAL,
-    userId INT NOT NULL references users(userId),
-    productId INT NOT NULL references products(productId)
-  )`;
-  try {
-    const result = productDB.query(queryString);
-    console.log(result);
-  } catch (err) {
-    console.log(err);
-  }
-}
+// async function usersToProductsTable() {
+//   let queryString = `
+//   CREATE TABLE userstoproducts (
+//     usersToProducts_id SERIAL,
+//     userId INT NOT NULL references users(userId),
+//     productId INT NOT NULL references products(productId)
+//   )`;
+//   try {
+//     const result = productDB.query(queryString);
+//     console.log(result);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
 //CREATE SESSIONS TABLE:
-async function sessionsTable() {
-  let queryString = ` 
-  CREATE TABLE sessions (
-    sessions_id SERIAL,
-    userId INT NOT NULL references users(userId),
-    ssid INT NOT NULL
-  )`;
-  try {
-    const result = productDB.query(queryString);
-    console.log(result);
-  } catch (err) {
-    console.log(err);
-  }
-}
+// async function sessionsTable() {
+//   let queryString = `
+//   CREATE TABLE sessions (
+//     sessions_id SERIAL,
+//     userId INT NOT NULL references users(userId),
+//     ssid INT NOT NULL
+//   )`;
+//   try {
+//     const result = productDB.query(queryString);
+//     console.log(result);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+//*////////////
 
 //INSERT ONTO PRODUCTS TABLE:
 // async function insertIntoTable() {
@@ -138,6 +141,92 @@ async function sessionsTable() {
 //   }
 // }
 
-// sessionsTable()
+// //! UPDATE******************************************************
 
+const productsTable = `
+  CREATE TABLE products (
+    _id SERIAL,
+    product_name VARCHAR NOT NULL,
+    image_url VARCHAR NOT NULL,
+    PRIMARY KEY (_id)
+  )`;
 
+const lowestDailyPriceTable = `
+  CREATE TABLE lowest_daily_price (
+    _id SERIAL,
+    product_id INT NOT NULL UNIQUE references products(_id),
+    date VARCHAR NOT NULL,
+    store_name VARCHAR NOT NULL,
+    lowest_daily_price FLOAT NOT NULL,
+    store_url VARCHAR NOT NULL,
+    PRIMARY KEY (_id)
+  )`;
+
+const usersTable = `
+//   INSERT INTO users ( hashedPassword, email) VALUES ( '45HYDH43H', 'chandnip6@gmail.com')
+//   `;
+
+const usersToProductsTable = ` 
+  CREATE TABLE users_to_products (
+    _id SERIAL,
+    user_id INT NOT NULL references users(_id),
+    product_id INT NOT NULL references products(_id),
+    PRIMARY KEY (_id)
+  )`;
+
+const sessionsTable = `
+CREATE TABLE sessions (
+  _id SERIAL,
+  user_id INT NOT NULL references users(_id),
+  ssid INT NOT NULL,
+  PRIMARY KEY (_id)
+)`;
+
+//CREATE TABLE:
+async function createTable(queryString) {
+  try {
+    const result = productDB.query(queryString);
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// createTable(productsTable)
+// createTable(lowestDailyPriceTable)
+// createTable(usersTable)
+// createTable(usersToProductsTable)
+// createTable(sessionsTable)
+
+const productsInsert = `
+INSERT INTO products (product_name, image_url) VALUES ( 'keyboard', 'www.keychron.com' )
+`;
+
+const lowestDailyPriceInsert = `
+INSERT INTO lowest_daily_price (product_id, date, store_name, lowest_daily_price, store_url ) VALUES (1, '12/17/2020', 'Best Buy', 120.23, 'www.bestbuy.com' )
+`;
+
+const usersInsert = `INSERT INTO users (email, password) VALUES ('test1@aol.com', 'monkey123')`;
+
+const sessionsInsert = `
+INSERT INTO sessions (user_id, ssid ) VALUES (1, 123)
+`;
+
+const usersToProductsInsert = `
+INSERT INTO users_to_products (user_id, product_id ) VALUES (1, 1)
+`;
+
+//INSERT INTO TABLE:
+function insertIntoTable(queryString) {
+  try {
+    const result = productDB.query(queryString);
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+insertIntoTable(productsInsert);
+insertIntoTable(lowestDailyPriceInsert);
+insertIntoTable(usersInsert);
+insertIntoTable(sessionsInsert);
