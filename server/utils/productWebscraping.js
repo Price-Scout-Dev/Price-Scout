77d881531b76a9f54346de7a46aea084a6e03dcc
Chandni Patel <chandnipatel@Chandnis-Mac-mini.local>
2020-12-21 20:52:05 -0500

    Added properties to object on getProductInfo function

--- server/utils/productWebscraping.js
+++ server/utils/productWebscraping.js
@@ -4,35 +4,38 @@ const getProductInfo = async (url) => {
   const browser = await puppeteer.launch({
     args: ["--disabled-setuid-sandbox", "--no-sandbox"],
   });
+
   const page = await browser.newPage();
   await page.goto(url);
 
+const productInfo = {};
 
   //1. Get lowestDailyPrice 
   await page.waitForSelector(".g9WBQb");
-  const price = await page.$eval(".g9WBQb", (el) => el.innerHTML);
-  console.log("Price: ", price);
+  productInfo.price = await page.$eval(".g9WBQb", (el) => el.innerHTML);
+  
+  //2. Get productName:
+  productInfo.productName = await page.$eval(".BvQan", (el) => el.innerHTML);
 
-  //2. Get storeName where lowestDailyPrice is from. 
-  const storeName = await page.$eval(".BvQan", (el) => el.innerHTML);
+  // 3. Get storeUrl (NOT WORKING):
+  const storeUrl = await page.$eval("a.shntl[href]", (el) => el.getAttribute('href'));
+  productInfo.storeUrl = `https://www.google.com/${storeUrl}`
+ 
+  //TODO: fix storeName:
+  //4. Get storeName:
+  const storeName = await page.$eval(".b5ycib", (el) => el.innerText);
   console.log("storeName: ", storeName);
+  
+  //5. Get productImageUrl:
+  productInfo.productImageUrl = await page.$eval("img.sh-div__image[src]", (el) => el.getAttribute('src'));
 
-  //3. Get storeUrl of the store 
-
-
-  //4. Get productName 
-
-
-
-  //5. Get productImageUrl 
-
-
-
-
-
+  
+  // console.log('productInfo OBJECT: ', productInfo);
+  browser.close();
+  console.log("browser closed")
 };
 
 getProductInfo('https://www.google.com/shopping/product/9333373489557772337/')
 //sample product page: https://www.google.com/shopping/product/9333373489557772337/
 
-module.exports = getProductInfo;
+module.exports = getProductInfo
