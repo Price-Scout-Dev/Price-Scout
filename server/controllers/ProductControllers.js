cd7d92524424901a930468cc33a50c27cb688629
Chandni Patel <chandnipatel@Chandnis-Mac-mini.local>
2020-12-22 21:25:34 -0500

    added addProduct Controller

--- server/controllers/ProductControllers.js
+++ server/controllers/ProductControllers.js
@@ -2,42 +2,36 @@ const priceTrackerDB = require("../models/priceTrackerModel.js");
 
 const productController = {};
 
-//Get Products Controller:
+//Get Products Controller- GET Request:
 productController.getProducts = (req, res, next) => {
-    return next()
-}
+  return next();
+};
 
-//Add Product Controller:
+//Add Product Controller- POST Request:
 productController.addProduct = (req, res, next) => {
-    const {productName, imageUrl} =req.body
-
-        let queryString = `
+  const { productName, imageUrl } = req.body;
+  let addProduct = `
         INSERT INTO products ( product_name, image_url) VALUES ($1, $2) RETURNING *
-        `; 
-        let values = [productName, imageUrl];
-    
-        priceTrackerDB
-          .query(queryString, values)
-          .then((data) => {
-           res.locals.products ={};
-           res.locals.products.productId = data.rows[0]._id;
-           res.locals.products.productName = data.rows[0].product_name;
-           res.locals.products.imageUrl = data.rows[0].image_url
-            return next();
-          })
-          .catch((err) => {
-            console.log(err);
-            return next(err);
-          });
-    
-}
+        `;
+  let values = [productName, imageUrl];
+  priceTrackerDB
+    .query(addProduct, values)
+    .then((data) => {
+      res.locals.products = {};
+      res.locals.products.productId = data.rows[0]._id;
+      res.locals.products.productName = data.rows[0].product_name;
+      res.locals.products.imageUrl = data.rows[0].image_url;
+      return next();
+    })
+    .catch((err) => {
+      console.log(err);
+      return next(err);
+    });
+};
 
-
-//Delete Product Controller:
+//Delete Product Controller- DELETE Request:
 productController.deleteProduct = (req, res, next) => {
-    return next()
-}
-
-
+  return next();
+};
 
 module.exports = productController;
--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -2,35 +2,29 @@ const express = require("express");
 const productRouter = express.Router();
 const productController = require("../controllers/productControllers");
 
-
 //Get All Products:
 //GET Request
 
-productRouter.get("/products", productController.getProducts , (req, res) => {
-    res.status(200).json("Get products");
-  });
+productRouter.get("/products", productController.getProducts, (req, res) => {
+  res.status(200).json("Get products");
+});
 
-
-//Add Product:
+//Add One Product:
 //POST Request
 
- 
-
 productRouter.post("/products", productController.addProduct, (req, res) => {
-    res.status(200).json(res.locals.products);
-  });
+  res.status(200).json(res.locals.products);
+});
 
 //Delete One Product:
 //DELETE Request
 
-productRouter.delete("/products/:id", productController.deleteProduct, (req, res) => {
+productRouter.delete(
+  "/products/:id",
+  productController.deleteProduct,
+  (req, res) => {
     res.status(200).json("Delete product");
-  });
-
-
-
-
-
-
+  }
+);
 
 module.exports = productRouter;
