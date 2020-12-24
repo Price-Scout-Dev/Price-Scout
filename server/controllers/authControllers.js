84bf871e0c0312df33eff89526f641c93c99bc92
christygomez <christygomez@gmail.com>
2020-12-23 21:38:12 -0500

    added bcrypt hashedPassword to createUser controller

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,16 +1,18 @@
-const priceTrackerDB = require("../models/priceTrackerModel.js");
-const bcrypt = require("bcryptjs");
+const priceTrackerDB = require('../models/priceTrackerModel.js');
+const bcrypt = require('bcryptjs');
 
 const authController = {};
 
 //Create User Controller:
-authController.createUser = (req, res, next) => {
+authController.createUser = async (req, res, next) => {
   if (req.body.email.length > 0 && req.body.password.length > 0) {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
     //create query string. insert user into the user table.
     let queryString = `
     INSERT INTO users ( email, password) VALUES ($1, $2) RETURNING *
     `; // parameterized sql query
-    let values = [req.body.email, req.body.password];
+    let values = [req.body.email, hashedPassword];
 
     priceTrackerDB
       .query(queryString, values)
@@ -26,8 +28,8 @@ authController.createUser = (req, res, next) => {
         return next(err);
       });
   } else {
-    console.log("password or username rejected");
-    res.status(418).json({ error: "invalid email or password" });
+    console.log('password or username rejected');
+    res.status(418).json({ error: 'invalid email or password' });
   }
 };
 
@@ -37,7 +39,7 @@ authController.setSSIDCookie = (req, res, next) => {
   let randomNumber = Math.floor(Math.random() * 1000000);
   let options = { maxAge: 90000000, httpOnly: true };
 
-  res.cookie("ssid", randomNumber, options);
+  res.cookie('ssid', randomNumber, options);
 
   //second, save the ssid into the database.
 
@@ -49,7 +51,7 @@ authController.setSSIDCookie = (req, res, next) => {
   priceTrackerDB
     .query(queryString, values)
     .then((data) => {
-      console.log("Session added: ", data);
+      console.log('Session added: ', data);
       return next();
     })
     .catch((err) => {
@@ -64,30 +66,30 @@ authController.setSSIDCookie = (req, res, next) => {
 authController.verifyUser = (req, res, next) => {
   //check database if the email exists. If the email exists, check if the password is correct. Query first.
 
-    //create query string. insert user into the user table.
-    let queryString = `
+  //create query string. insert user into the user table.
+  let queryString = `
     SELECT * FROM users WHERE email=$1 and password=$2
     `; // parameterized sql query
-    let values = [req.body.email, req.body.password];
+  let values = [req.body.email, req.body.password];
 
-    priceTrackerDB
-      .query(queryString, values)
-      .then((data) => {
-        if (data.rows.length > 0) {
-          res.locals.loginInfo = {};
-          res.locals.loginInfo.userId = data.rows[0]._id;
-          res.locals.loginInfo.email = req.body.email;
-          return next();
-        } else {
-          //send error object to the front end.
-          console.log("invalid email or password");
-          res.status(200).json({ error: "invalid email or password" });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        return next(err);
-      });
+  priceTrackerDB
+    .query(queryString, values)
+    .then((data) => {
+      if (data.rows.length > 0) {
+        res.locals.loginInfo = {};
+        res.locals.loginInfo.userId = data.rows[0]._id;
+        res.locals.loginInfo.email = req.body.email;
+        return next();
+      } else {
+        //send error object to the front end.
+        console.log('invalid email or password');
+        res.status(200).json({ error: 'invalid email or password' });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      return next(err);
+    });
 };
 
 module.exports = authController;
