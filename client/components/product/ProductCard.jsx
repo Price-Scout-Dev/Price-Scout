716fd2d36f87310e7116cd72e8faf02841fb5053
Stanley Huang <iamstanleyhuang@gmail.com>
2020-12-26 13:41:05 -0800

    Add grid layout

--- client/components/product/ProductCard.jsx
+++ client/components/product/ProductCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid, Card } from '@material-ui/core';
 
 const ProductCard = ({
 	productName,
@@ -13,14 +14,16 @@ const ProductCard = ({
 	};
 
 	return (
-		<div>
-			<h4>Product: {productName}</h4>
-			<img src={imageUrl} />
-			<h5>${productPrice}</h5>
-			<p>Retailer: {storeName}</p>
-			<h6>id: {productId}</h6>
-			<button onClick={handleClick}>Stop Tracking</button>
-		</div>
+		<Grid item xs={12} sm={6} md={4} lg={3}>
+			<Card>
+				<h4>Product: {productName}</h4>
+				<img src={imageUrl} />
+				<h5>${productPrice}</h5>
+				<p>Retailer: {storeName}</p>
+				<h6>id: {productId}</h6>
+				<button onClick={handleClick}>Stop Tracking</button>
+			</Card>
+		</Grid>
 	);
 };
 
