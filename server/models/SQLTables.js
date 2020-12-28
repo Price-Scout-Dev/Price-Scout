e2facb3968e991969b80288645534452204bad45
christygomez <christygomez@gmail.com>
2020-12-27 22:31:54 -0500

    getProducts controller complete

--- server/models/SQLTables.js
+++ server/models/SQLTables.js
@@ -1,147 +1,5 @@
-const productDB = require("./priceTrackerModel");
-
-//CREATE PRODUCTS TABLE:
-// async function createProductsTable() {
-//   let queryString = `
-//   CREATE TABLE sessions (
-//     _id SERIAL,
-//     user_id INT NOT NULL references users(_id),
-//     ssid INT NOT NULL,
-//     PRIMARY KEY (_id)
-//   )`;
-//   try {
-//     const result =  productDB.query(queryString);
-//     console.log(result);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-// createProductsTable()
-//CREATE LOWEST DAILY PRICE TABLE:
-// async function createLowestDailyPriceTable() {
-//   let queryString = `
-//   CREATE TABLE lowestDailyPrice (
-//     lowestDailyPrice_id SERIAL,
-//     productId INT NOT NULL UNIQUE references products(productId),
-//     date VARCHAR NOT NULL,
-//     storeName VARCHAR NOT NULL,
-//     lowestDailyPrice FLOAT NOT NULL,
-//     storeUrl VARCHAR NOT NULL,
-//     PRIMARY KEY (lowestDailyPrice_id)
-//   )`;
-//   try {
-//     const result = productDB.query(queryString);
-//     console.log(result);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-// CREATE USERS TABLE:
-// async function createUsersTable() {
-//   let queryString = `
-//   CREATE TABLE users (
-//     userId SERIAL,
-//     hashedPassword VARCHAR NOT NULL,
-//     email VARCHAR NOT NULL,
-//     PRIMARY KEY (userId)
-//   )`;
-//   try {
-//     const result = productDB.query(queryString);
-//     console.log(result);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-// CREATE USER TO PRODUCT TABLE:
-// async function usersToProductsTable() {
-//   let queryString = `
-//   CREATE TABLE userstoproducts (
-//     usersToProducts_id SERIAL,
-//     userId INT NOT NULL references users(userId),
-//     productId INT NOT NULL references products(productId)
-//   )`;
-//   try {
-//     const result = productDB.query(queryString);
-//     console.log(result);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-//CREATE SESSIONS TABLE:
-// async function sessionsTable() {
-//   let queryString = `
-//   CREATE TABLE sessions (
-//     sessions_id SERIAL,
-//     userId INT NOT NULL references users(userId),
-//     ssid INT NOT NULL
-//   )`;
-//   try {
-//     const result = productDB.query(queryString);
-//     console.log(result);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-//*////////////
-
-//INSERT ONTO PRODUCTS TABLE:
-// async function insertIntoTable() {
-//     let queryString = `
-//   INSERT INTO products VALUES (143, 'keyboard', 'www.keychron.com' )
-//   `;
-//   try {
-//     const result = productDB.query(queryString);
-//     console.log(result);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-//INSERT ONTO LOWEST DAILY PRICE TABLE:
-// async function insertIntoLowestDailyPriceTable() {
-//     let queryString = `
-//   INSERT INTO lowestDailyPrice (productId, date, storeName, lowestDailyPrice, storeUrl ) VALUES (123, '12/17/2020', 'Best Buy', 120.23, 'www.bestbuy.com' )
-//   `;
-//   try {
-//     const result = productDB.query(queryString);
-//     console.log(result);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-//INSERT ONTO USERS TABLE:
-// async function insertUsersTable() {
-//     let queryString = `
-//   INSERT INTO users ( hashedPassword, email) VALUES ( '45HYDH43H', 'chandnip6@gmail.com')
-//   `;
-//   try {
-//     const result = productDB.query(queryString);
-//     console.log(result);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-// INSERT ONTO USERS To PRODUCT TABLE:
-// async function insertUserToProductTable() {
-//   let queryString = `
-//     INSERT INTO userstoproducts (userID, productID) VALUES ( '', '1', '123')
-//   `;
-//   try {
-//     const result = productDB.query(queryString);
-//     console.log(result);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-// //! UPDATE******************************************************
+const priceTrackerDB = require('./priceTrackerModel.js');
+// const dotenv = require('dotenv').config();
 
 const productsTable = `
   CREATE TABLE products (
@@ -170,7 +28,7 @@ const usersTable = `
    password VARCHAR,
    PRIMARY KEY (_id)
  )
- `
+ `;
 
 const usersToProductsTable = ` 
   CREATE TABLE users_to_products (
@@ -189,20 +47,42 @@ CREATE TABLE sessions (
 )`;
 
 //CREATE TABLE:
-async function createTable(queryString) {
-  try {
-    const result = productDB.query(queryString);
-    console.log(result);
-  } catch (err) {
-    console.log(err);
-  }
-}
+// async function createTable(queryString) {
+//   try {
+//     const result = await priceTrackerDB.query(queryString);
+//     // console.log(result);
+//   } catch (err) {
+//     // console.log(err);
+//   }
+// }
+
+function testTable() {
+const queryString = `CREATE TABLE test (
+      _id SERIAL,
+      product VARCHAR
+    )`
+  priceTrackerDB
+    .query(queryString)
+    .then(result =>
+      console.log(result)
+      )
+    .catch(err=> console.log("ERROR: " + err))
+  } 
+
+const test = `
+CREATE TABLE test (
+  _id SERIAL
+)`;
+
+testTable();
 
 // createTable(productsTable)
 // createTable(lowestDailyPriceTable)
 // createTable(usersTable)
 // createTable(usersToProductsTable)
 // createTable(sessionsTable)
+
+// createTable(test);
 
 const productsInsert = `
 INSERT INTO products (product_name, image_url) VALUES ( 'keyboard', 'www.keychron.com' )
@@ -223,16 +103,20 @@ INSERT INTO users_to_products (user_id, product_id ) VALUES (1, 1)
 `;
 
 //INSERT INTO TABLE:
-function insertIntoTable(queryString) {
-  try {
-    const result = productDB.query(queryString);
-    console.log(result);
-  } catch (err) {
-    console.log(err);
-  }
-}
+// function insertIntoTable(queryString) {
+//   try {
+//     const result = priceTrackerDB.query(queryString);
+//     console.log(result);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
 // insertIntoTable(productsInsert);
 // insertIntoTable(lowestDailyPriceInsert);
 // insertIntoTable(usersInsert);
 // insertIntoTable(sessionsInsert);
+
+
+// sql query to get all prodcuts based on user id
+// SELECT users_to_products.user_id, products.product_name FROM users_to_products INNER JOIN products ON users_to_products.product_id=products._id WHERE users_to_products.user_id=2
