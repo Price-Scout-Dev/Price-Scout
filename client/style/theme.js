fbb1587ce9a3627b7356b957dee0bfc9be8949ba
Stanley Huang <iamstanleyhuang@gmail.com>
2020-12-28 02:22:23 -0800

    Add registration form theme

--- client/style/theme.js
+++ client/style/theme.js
@@ -4,7 +4,8 @@ const useStyles = makeStyles((theme) => ({
 	root: {
 		minHeight: '100vh',
 		paddingTop: '15rem',
-		background: 'linear-gradient(to bottom right, #FFFFFF, #E0E0E0)',
+		// background: 'linear-gradient(to right, #FFFFFF, #E0E0E0)',
+		background: 'linear-gradient(to right, #FFFFFF, #B2FEFA)',
 	},
 	loginBox: {
 		width: '100%',
@@ -24,6 +25,13 @@ const useStyles = makeStyles((theme) => ({
 		alignItems: 'center',
 		padding: '3rem 1.5rem',
 		borderRadius: '2rem',
+		[theme.breakpoints.down(913)]: {
+			marginTop: '2rem',
+		},
+	},
+	loginForm: {
+		display: 'flex',
+		flexDirection: 'column',
 	},
 	loginTextField: {
 		marginBottom: '0.5rem',
@@ -35,15 +43,36 @@ const useStyles = makeStyles((theme) => ({
 	},
 	loginDivider: {
 		width: '20rem',
-		margin: '1.5rem 0rem',
+		margin: '1.5rem 0',
 	},
 	loginCreateAccountBtn: {
 		background: '#42B72A',
 		color: 'white',
 		'&:hover': {
 			background: '#359222',
-			transition: '250ms',
 		},
+	},
+	registerForm: {
+		padding: '1.5rem 3rem 3rem',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	registerDivider: {
+		width: '5rem',
+		margin: '0.3rem 0 1.5rem',
+	},
+	registerBtn: {
+		background: 'linear-gradient(to bottom right, #F12711, #F5AF19)',
+		color: 'white',
+		'&:hover': {
+			background: 'linear-gradient(to bottom right, #D9230F, #DB9D16)',
+		},
+		marginTop: '1rem',
+		borderRadius: '2rem',
+		width: '50%',
+		alignSelf: 'center',
 	},
 }));
 
