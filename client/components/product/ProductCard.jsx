723dd197f574fcb6388d81c9bc047f47d8290f78
Stanley Huang <iamstanleyhuang@gmail.com>
2020-12-28 17:28:34 -0800

    Style product card

--- client/components/product/ProductCard.jsx
+++ client/components/product/ProductCard.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
-import { Grid, Card } from '@material-ui/core';
+import {
+	Grid,
+	Card,
+	CardActionArea,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Button,
+	Typography,
+} from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import useStyles from '../../style/theme';
 
 const ProductCard = ({
 	productName,
@@ -8,21 +19,41 @@ const ProductCard = ({
 	productPrice,
 	productId,
 	deleteProduct,
-	date,
 }) => {
 	const handleClick = () => {
 		deleteProduct(productId);
 	};
 
+	const classes = useStyles();
+
 	return (
 		<Grid item xs={12} sm={6} md={4} lg={3}>
-			<Card>
-				<h4>Product: {productName}</h4>
-				<img src={imageUrl} />
-				<h5>${productPrice}</h5>
-				<p>Retailer: {storeName}</p>
-				<h6>id: {productId}</h6>
-				<button onClick={handleClick}>Stop Tracking</button>
+			<Card className={classes.productCard}>
+				<CardActionArea>
+					<CardMedia
+						className={classes.productCardMedia}
+						image={imageUrl}
+						title={productName}
+					/>
+				</CardActionArea>
+				<CardContent>
+					<Typography variant="h5">Product: {productName}</Typography>
+					<h5>${productPrice}</h5>
+					<p>Retailer: {storeName}</p>
+					<h6>id: {productId}</h6>
+				</CardContent>
+				<CardActions>
+					<Button
+						onClick={handleClick}
+						variant="contained"
+						color="secondary"
+						size="small"
+						startIcon={<DeleteIcon />}
+						style={{ margin: '0 auto' }}
+					>
+						Delete
+					</Button>
+				</CardActions>
 			</Card>
 		</Grid>
 	);
