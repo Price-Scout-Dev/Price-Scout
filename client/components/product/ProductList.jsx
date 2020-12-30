e5166344fabcf225e6a1db603d99d737729c7e10
Stanley Huang <iamstanleyhuang@gmail.com>
2020-12-29 18:25:16 -0800

    Rename parameters from camelCase to snake_case

--- client/components/product/ProductList.jsx
+++ client/components/product/ProductList.jsx
@@ -3,26 +3,20 @@ import ProductCard from './ProductCard';
 import { v4 as uuidv4 } from 'uuid';
 
 const ProductList = ({ list, deleteProduct }) => {
+	console.log('list in ProductList.jsx', list);
 	const productItems = list.map(
-		({ productName, imageUrl, storeName, productPrice, productId, date }) => {
-			console.log({
-				productName,
-				imageUrl,
-				storeName,
-				productPrice,
-				productId,
-				date,
-			});
+		({ product_name, image_url, store_name, lowest_daily_price, product_id, timestamp }) => {
 			//wrap in Link for detail route, if so
 			return (
 				<ProductCard
-					productId={productId}
+					productId={product_id}
 					key={uuidv4()}
-					productName={productName}
-					imageUrl={imageUrl}
-					storeName={storeName}
-					productPrice={productPrice}
+					productName={product_name}
+					imageUrl={image_url}
+					storeName={store_name}
+					productPrice={lowest_daily_price}
 					deleteProduct={deleteProduct}
+					timestamp={timestamp}
 				/>
 			);
 		}
