f0c56ab677d7b8c3b2297de97a03784426765877
Stanley Huang <iamstanleyhuang@gmail.com>
2020-12-31 12:48:51 -0800

    Add NavBar.jsx

---
+++ client/components/nav/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { AppBar, Button, Typography, Toolbar } from '@material-ui/core';
+import { AccountCircle } from '@material-ui/icons';
+import useStyles from '../../style/theme';
+
+const NavBar = ({ email, logOut }) => {
+	const classes = useStyles();
+
+	return (
+		<AppBar>
+			<Toolbar className={classes.productAppBar}>
+				<div className={classes.navBar}>
+					<AccountCircle />
+					<Typography className={classes.username} variant="h6">
+						{email}
+					</Typography>
+				</div>
+				<Button onClick={logOut} color="inherit">
+					Logout
+				</Button>
+			</Toolbar>
+		</AppBar>
+	);
+};
+
+export default NavBar;
